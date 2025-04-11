a48781837c54ba7f0943a2f64cdb59e601143804
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-11 19:38:55 +0500

    up

--- js/main.js
+++ js/main.js
@@ -1,1 +1,47 @@
-console.log("Hello");
+// 1-vazifa Ball baholash
+// const input = +prompt("Bahoni kiriting?")
+// if (input > 0 && input < 60) {
+//     console.log("Qoniqarsiz");
+// } else if (input < 70) {
+//     console.log("Qoniqarli");
+// } else if (input < 90) {
+//     console.log("Yaxshi");
+// } else if (input <= 100) {
+//     console.log("A'lo");
+// }
+
+// 2-vazifa Hafta kunlari
+// const day = +prompt("Raqam kiriting?")
+
+// if (day == 1) {
+//     console.log("Dushanba");
+// } else if (day == 2) {
+//     console.log("Seshanba");
+// } else if (day == 3) {
+//     console.log("Chorshanba");
+// } else if (day == 4) {
+//     console.log("Payshanba");
+// } else if (day == 5) {
+//     console.log("Juma");
+// } else if (day == 6) {
+//     console.log("Shanba");
+// } else if (day == 7) {
+//     console.log("Yakshanba");
+// } else {
+//     console.log("Noto'g'ri raqam");
+// }
+
+// 3-vazifa Yoshga qarab toifa aniqlash
+// const yosh = +prompt("Raqam kiriting?")
+
+// if (yosh >= 0 && yosh < 13) {
+//     console.log("Bola");
+// } else if (yosh < 20) {
+//     console.log("O'smir");
+// } else if (yosh < 60) {
+//     console.log("Katta");
+// } else if (yosh < 120) {
+//     console.log("Qariya");
+// } else {
+//     console.log("Noto'g'ri raqam");
+// }
---
+++ js/test.js
@@ -0,0 +1,14 @@
+// 3-vazifa Yoshga qarab toifa aniqlash
+// const yosh = +prompt("Raqam kiriting?")
+
+// if (yosh >= 0 && yosh < 13) {
+//     console.log("Bola");
+// } else if (yosh < 20) {
+//     console.log("O'smir");
+// } else if (yosh < 60) {
+//     console.log("Katta");
+// } else if (yosh < 120) {
+//     console.log("Qariya");
+// } else {
+//     console.log("Noto'g'ri raqam");
+// }
