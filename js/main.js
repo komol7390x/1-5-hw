ff7f18d7a744a2036dceb7142a6609b36a425ee0
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-11 20:00:09 +0500

    up

--- js/main.js
+++ js/main.js
@@ -55,3 +55,19 @@
 // firstNumber > secondNumber ?
 //     console.log("Birinchi raqam katta") : (secondNumber > thirdNumber ?
 //         console.log("Ikkinchi raqam katta") : console.log("Uchinchi raqam katta"))
+
+// 5-vazifa Oy kunlari
+
+// const month = +prompt("Raqam kiriting?")
+// const toq = [1, 3, 5, 7, 8, 10, 12]
+// const juft = [4, 6, 9, 11]
+
+// if (toq.includes(month)) {
+//     console.log("31");
+// } else if (juft.includes(month)) {
+//     console.log("30");
+// } else if (month === 2) {
+//     console.log("28");
+// } else {
+//     console.log("Noto'g'ri raqam");
+// }
--- js/test.js
+++ js/test.js
@@ -1,9 +1,15 @@
-// 4-vazifa Katta son
+// 5-vazifa Oy kunlari
 
-// const firstNumber = +prompt("Birinchi soni kiriting")
-// const secondNumber = +prompt("Ikkinchi soni kiriting")
-// const thirdNumber = +prompt("Uchinchi soni kiriting")
+// const month = +prompt("Raqam kiriting?")
+// const toq = [1, 3, 5, 7, 8, 10, 12]
+// const juft = [4, 6, 9, 11]
 
-// firstNumber > secondNumber ?
-//     console.log("Birinchi raqam katta") : (secondNumber > thirdNumber ?
-//         console.log("Ikkinchi raqam katta") : console.log("Uchinchi raqam katta"))
+// if (toq.includes(month)) {
+//     console.log("31");
+// } else if (juft.includes(month)) {
+//     console.log("30");
+// } else if (month === 2) {
+//     console.log("28");
+// } else {
+//     console.log("Noto'g'ri raqam");
+// }
