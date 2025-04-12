574f33e084bbd84e0694ba895f2f1017a3be55eb
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-12 11:53:25 +0500

    up

--- js/main.js
+++ js/main.js
@@ -220,3 +220,18 @@
 // } else {
 //     console.log(num);
 // }
+
+// 19-vazifa Raqamlar o'rnini almashtirish
+// let num = +prompt("Raqam kiriting?")
+// let num1=""
+// while (num > 0) {
+//     let a = num % 10
+//     if (a % 2 == 0) {
+//         num1+=0
+//     } else {
+//         num1+=a
+//     }
+//     num=Math.floor(num/10)
+// }
+// num1 = +(num1.split("").reverse().join(""))
+// console.log(num1);
--- js/test.js
+++ js/test.js
@@ -1,11 +1,16 @@
-// 18-vazifa Sonlar ketma-ketligi
-// const num = +prompt("Raqam kiriting?")
-// if (num % 3 == 0 && num % 5 == 0) {
-//     console.log("FizzBuzz");
-// } else if (num % 3 == 0) {
-//     console.log("Buzz");
-// } else if (num % 5 == 0) {
-//     console.log("Fizz");
-// } else {
-//     console.log(num);
+// 18-vazifa Raqamlar o'rnini almashtirish
+// let num = +prompt("Raqam kiriting?")
+// let num1=""
+// while (num > 0) {
+//     let a = num % 10
+//     if (a % 2 == 0) {
+//         num1+=0
+//     } else {
+//         num1+=a
+//     }
+//     num=Math.floor(num/10)
 // }
+// num1 = +(num1.split("").reverse().join(""))
+// console.log(num1);
+
+
