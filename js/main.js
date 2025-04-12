910c464030ccb772a0c81edb187cc071ab2e6ad9
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-12 12:03:32 +0500

    up

--- js/main.js
+++ js/main.js
@@ -235,3 +235,14 @@
 // }
 // num1 = +(num1.split("").reverse().join(""))
 // console.log(num1);
+
+// 20-vazifa Ichma-ich loop
+// let num = +prompt("Raqam kiriting?")
+// let sum=1
+// for (let i = 0; i < num; i++){
+//     for (let k = 0; k < num; k++){
+//         console.log(sum);
+//         sum+=1
+//     }
+//     console.log("\n");
+// }
--- js/test.js
+++ js/test.js
@@ -1,16 +1,11 @@
-// 18-vazifa Raqamlar o'rnini almashtirish
+// 18-vazifa Ichma-ich loop
 // let num = +prompt("Raqam kiriting?")
-// let num1=""
-// while (num > 0) {
-//     let a = num % 10
-//     if (a % 2 == 0) {
-//         num1+=0
-//     } else {
-//         num1+=a
+// let sum=1
+// for (let i = 0; i < num; i++){
+//     for (let k = 0; k < num; k++){
+//         console.log(sum);
+//         sum+=1
 //     }
-//     num=Math.floor(num/10)
+//     console.log("\n");
 // }
-// num1 = +(num1.split("").reverse().join(""))
-// console.log(num1);
-
 
