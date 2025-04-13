33580cde8d68aa3b76b4ee7fa1a05a60fcdbaf97
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-13 09:31:04 +0500

    up

--- js/main.js
+++ js/main.js
@@ -246,3 +246,20 @@
 //     }
 //     console.log("\n");
 // }
+
+// 21-vazifa O'yin Personaji
+// function createCharacter(ism, kuch) {
+//     const daraja = Math.floor(Math.random() * 10) + 1;
+//     character = {
+//         name: ism,
+//         power: kuch,
+//         level: daraja,
+//         info:`${daraja}-darajali ${ism}, kuchi: ${kuch}`
+//     }
+//     return character
+// }
+
+// const name1 = prompt("Qahramon ismini kiriting?")
+// const power1 = prompt("Qahramon kuchini kiriting?")
+// const info1=createCharacter(name1,power1)
+// console.log(info1);
--- js/test.js
+++ js/test.js
@@ -1,11 +1,19 @@
-// 18-vazifa Ichma-ich loop
-// let num = +prompt("Raqam kiriting?")
-// let sum=1
-// for (let i = 0; i < num; i++){
-//     for (let k = 0; k < num; k++){
-//         console.log(sum);
-//         sum+=1
+// 21-vazifa O'yin Personaji
+// function createCharacter(ism, kuch) {
+//     const daraja = Math.floor(Math.random() * 10) + 1;
+//     character = {
+//         name: ism,
+//         power: kuch,
+//         level: daraja,
+//         info:`${daraja}-darajali ${ism}, kuchi: ${kuch}`
 //     }
-//     console.log("\n");
+//     return character
 // }
+
+// const name1 = prompt("Qahramon ismini kiriting?")
+// const power1 = prompt("Qahramon kuchini kiriting?")
+// const info1=createCharacter(name1,power1)
+// console.log(info1);
+
+
 
