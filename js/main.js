bbc23dd80b6f7a4db6c61a240c1b7b8dab47819a
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-14 09:51:40 +0500

    up

--- js/main.js
+++ js/main.js
@@ -356,3 +356,28 @@
 // let info = new rateMovie("Avatar", 2009, [9, 8, 10, 7, 9])
 // console.log((info));
 
+// 28-vazifa Git Commit
+// function randCommit() {
+//     const str = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890"
+//     let commit = ""
+//     let saveCommit = []
+//     for (let i = 0; i < 10; i++){
+//         commit += str[Math.floor(Math.random() * str.length)];
+//     }
+//     if (!saveCommit.includes(commit)) {
+//         saveCommit.push(commit);
+//         return commit;
+//     } else {
+//         return randCommit()
+//     }
+// }
+// function createCommit(message, files) {
+//     let id = randCommit();
+//     this.message = message;
+//     this.files = files;
+//     this.date = "2024-03-15";
+//     this.id = id;
+//     this.author="User"
+// }
+// let info = new createCommit("Add new features", ["app.js", "style.css"])
+// console.log(info);
